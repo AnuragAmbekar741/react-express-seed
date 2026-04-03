d20160b61b2002e1b30816b383df03cdf328ae69
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2026-04-02 18:07:39 -0700

    react router added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import "./global.css";
+import { Route, Routes } from "react-router-dom";
+
+const SlashRoute = () => (
+  <div className="h-screen w-screen flex flex-col justify-center items-center">
+    <h1 className="text-2xl">React powered by vite</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl">Hello, this is vite react app running...</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={SlashRoute()} />
+    </Routes>
   );
 }
 
